6bcf97dc604f1871cb9620af1a4d54ed9beb45ab
Alaykumar Patel <alay1431@gmail.com>
2018-07-21 21:02:10 -0400

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM golang:1.8
 WORKDIR /go/src/github.com/alaypatel07/tracer
 COPY . .
 
+RUN go get -u github.com/golang/dep/cmd/dep
 RUN go get -d -v ./...
+RUN dep ensure -v
 RUN go install -v ./...
 
 CMD ["tracer"]
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "github.com/google/gopacket/pcap"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("Running packet capture on eth0")
+	handle, err := pcap.OpenLive("eth0", 1600, true, pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	}
 }
